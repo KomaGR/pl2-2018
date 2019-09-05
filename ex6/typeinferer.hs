9710451e8700eb7c93c088b4f535658464c0e06d
Orestis Kaparounakis <orestis.kapar@hotmail.com>
2019-09-05 11:28:53 +0300

    Add sources, give up on state monad

--- ex6/typeinferer.hs
+++ ex6/typeinferer.hs
@@ -1,6 +1,8 @@
 import Data.Char
 import System.IO
 import Text.Read
+import qualified Data.Map.Lazy as Map
+import qualified Control.Monad.State.Lazy as State
 
 data Type  =  Tvar Int | Tfun Type Type                        deriving Eq
 data Expr  =  Evar String | Eabs String Expr | Eapp Expr Expr  deriving Eq
@@ -47,13 +49,33 @@ instance Show Type where
 readExpr :: String -> Expr
 readExpr s = read s :: Expr
 
--- typist :: Expr -> Maybe Type
-typist Evar a = x
-typist Eabs a e = x
-typist Eapp e1 e2 = x
+aux_lookup m a = case (Map.lookup a m) of 
+                  Just number -> (m, number)
+                  Nothing -> (Map.insert a number m, number)
+                                    where number = Map.size m
+
+typist :: Map.Map String Int -> Expr -> (Map.Map String Int, Type)
+-- = "(Evar " ++ a ++ ")"
+typist m x@(Evar a) = Tvar number
+                      where (_, number) = aux_lookup m a            
+
+-- = "(Eabs " ++ a ++ " " ++ typist e ++ ")" 
+typist m x@(Eabs a e) = Tfun (Tvar number) (typist m1 e)
+                      where (m1, number) = aux_lookup m a
+
+-- = "(Eapp " ++ typist e1 ++ " & " ++ typist e2 ++ ")"
+typist m x@(Eapp e1 e2) = Tvar t1
+                          where (m1, t1) = typist m e1
+                                (m2, t2) = typist m1 e1
 
 -- Solve for each
 solve :: String -> String
-solve = show . typist . readExpr
+solve = show . typist Map.empty . readExpr
 
 main = interact $ unlines . map solve . tail . lines
+
+-- Sources:
+-- http://learnyouahaskell.com/making-our-own-types-and-typeclasses
+-- https://eli.thegreenplace.net/2018/type-inference/
+-- https://eli.thegreenplace.net/2018/unification/
+-- https://mmhaskell.com/monads-5
