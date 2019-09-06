16e1dda098eaf5fb6c48a790cd9a51a5a199e60d
Orestis Kaparounakis <orestis.kapar@hotmail.com>
2019-09-06 19:30:49 +0300

    Add unification

--- ex6/typeinferer.hs
+++ ex6/typeinferer.hs
@@ -65,31 +65,53 @@ typist x@(Evar a) = do
                       State.return $ Tvar n_prime
 
 -- = "(Eabs " ++ a ++ " " ++ typist e ++ ")"
-typist x@(Eabs a e) = do 
-                        t2 <- typist e
-                        (m,n,c) <- State.get
-                        let (m1, n_prime, new_n) = aux_lookup m a n
-                        State.put (m1, new_n, c)
-                        State.return $ (Tfun (Tvar n_prime) (t2))
+typist x@(Eabs a e) = do
+  (m,n,c) <- State.get
+  let (m1, n_prime, new_n) = aux_lookup m a n
+  State.put (m1, new_n, c)
+  t2 <- typist e
+  State.return $ (Tfun (Tvar n_prime) (t2))
 
 -- = "(Eapp " ++ typist e1 ++ " & " ++ typist e2 ++ ")"
 typist x@(Eapp e1 e2) = do 
-                          t2 <- typist e2
-                          (m,n,c) <- State.get
-                          let a = Tvar n
-                          State.put (m,n+1,c)   -- create new var a for t1 == t2->a
-                          t1 <- typist e1
-                          (m1,n1,c1) <- State.get
-                          State.put (m1, n1, (t1, Tfun t2 a):c1)
-                          State.return $ a
-                        
-                                
-
+  t1 <- typist e1
+  t2 <- typist e2
+  (m,n,c) <- State.get
+  let a = Tvar n        -- create new var a for t1 == t2->a
+  State.put (m,n+1,c)     
+  (m1,n1,c1) <- State.get
+  State.put (m1, n1, (t1, Tfun t2 a):c1)
+  State.return $ a
+  
 typist_wrapper e = State.runState (typist e) (Map.empty, 0, [])
+
+-- Finds if t1 shows up in t2
+show_up :: Type -> Type -> Bool
+show_up t1 t2 | t1 == t2 = True
+show_up t1 (Tfun t21 t22) = or [show_up t1 t21, show_up t1 t22]
+show_up t1 (Tvar _) = False
+
+-- Apply substitution $s in constraint list $c
+subs c s
+
+unify :: [(Type, Type)] -> [(Type, Type)] -> Maybe [(Type, Type)]
+unify [] solution = solution
+unify (t1,t2):c sol | t1 == t2 = unify c
+unify (t1@(Tvar a), t2):c sol | not show_up t1 t2 = unify c_prime ((t1, t2):sol)
+unify (t1, t2@(Tvar a)):c sol | not show_up t2 t1 = unify c_prime ((t2, t1):sol)
+unify (t1@(Tfun t11 t12), t2@(Tfun t21 t22)):c sol = unify ((t11,t21):(t12,t22):c) sol
+unify _ = Nothing
+
+unify_wrapper (type, var_map, constraint_set) = 
+  case unify constraint_set [] of
+    Just sol -> sol -- subs on $type
+    Nothing -> 
+
+
 
 -- Solve for each
 solve :: String -> String
-solve = show . typist_wrapper . readExpr
+solve = show . unify_wrapper . typist_wrapper . readExpr
 
 main = interact $ unlines . map solve . tail . lines
 
