1369256728412565138cf0e5643d7063e609a13e
Orestis Kaparounakis <orestis.kapar@hotmail.com>
2019-09-08 00:29:14 +0300

    Fix identation

--- ex6/typeinferer.hs
+++ ex6/typeinferer.hs
@@ -92,22 +92,22 @@ typist x@(Evar a) = do
 
 -- = "(Eabs " ++ a ++ " " ++ typist e ++ ")"
 typist x@(Eabs a e) = do
-  (m,n,c) <- State.get
-  let (m1, n_prime, new_n) = aux_lookup m a n
-  State.put (m1, new_n, c)
-  t2 <- typist e
-  State.return $ (Tfun (Tvar n_prime) (t2))
+                        (m,n,c) <- State.get
+                        let (m1, n_prime, new_n) = aux_lookup m a n
+                        State.put (m1, new_n, c)
+                        t2 <- typist e
+                        State.return $ (Tfun (Tvar n_prime) (t2))
 
 -- = "(Eapp " ++ typist e1 ++ " & " ++ typist e2 ++ ")"
 typist x@(Eapp e1 e2) = do 
-  t1 <- typist e1
-  t2 <- typist e2
-  (m,n,c) <- State.get
-  let a = Tvar n        -- create new var a for t1 == t2->a
-  State.put (m,n+1,c)     
-  (m1,n1,c1) <- State.get
-  State.put (m1, n1, (t1, Tfun t2 a):c1)
-  State.return $ a
+                          t1 <- typist e1
+                          t2 <- typist e2
+                          (m,n,c) <- State.get
+                          let a = Tvar n        -- create new var a for t1 == t2->a
+                          State.put (m,n+1,c)     
+                          (m1,n1,c1) <- State.get
+                          State.put (m1, n1, (t1, Tfun t2 a):c1)
+                          State.return $ a
   
 typist_wrapper e = State.runState (typist e) (Map.empty, 0, [])
 
@@ -146,8 +146,7 @@ unify ((t1@(Tfun t11 t12), t2@(Tfun t21 t22)):c) sol =
 
 unify ((_,_):_) _ = Nothing
 
-unify_wrapper :: (Type, (Map.Map String Int, Int, [(Type, Type)])) 
-                -> Maybe Type
+unify_wrapper :: (Type, (Map.Map String Int, Int, [(Type, Type)])) -> Maybe Type
 unify_wrapper (t, (var_map, _, constraint_set)) = 
   case (unify constraint_set []) of
     -- Just sol -> Just (t, sol) -- subs on $t(ype) with fold
