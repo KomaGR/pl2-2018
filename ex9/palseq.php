f1f089b99cfc46f0587187c6e01e85062bd7e6d7
Orestis Kaparounakis <orestis.kapar@hotmail.com>
2020-05-08 15:09:21 +0300

    Format document with phpfmt

--- ex9/palseq.php
+++ ex9/palseq.php
@@ -1,35 +1,45 @@
 <?php
-if ("${_SERVER['QUERY_STRING']}" == "")
+if ("${_SERVER['QUERY_STRING']}" == "") {
     $self = "${_SERVER['PHP_SELF']}";
-else
+} else {
     $self = "${_SERVER['PHP_SELF']}?${_SERVER['QUERY_STRING']}";
+}
 
-if (isset($_GET['cheat']))
+if (isset($_GET['cheat'])) {
     $cheat = true;
-else
+} else {
     $cheat = false;
+}
 
-    
-if (isset($_GET['limit']))
+if (isset($_GET['limit'])) {
     $limit = $_GET['limit'];
-else
-  $limit = 10;
-    
-if (isset($_GET['reset']))
-  session_reset();
-else
-  session_start();
+} else {
+    $limit = 10;
+}
 
+if (isset($_GET['reset'])) {
+    session_reset();
+} else {
+    session_start();
+}
 
-function prime($p) {
-  if ($p == 2)
-      return true;
-  if ($p % 2 == 0)
-      return false;
-  for ($i = 3; $i * $i <= $p; $i += 2)
-      if ($p % $i == 0)
-          return false;
-  return true;
+function prime($p)
+{
+    if ($p == 2) {
+        return true;
+    }
+
+    if ($p % 2 == 0) {
+        return false;
+    }
+
+    for ($i = 3; $i * $i <= $p; $i += 2) {
+        if ($p % $i == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 $permited_chars = "abcdefghijklmnopqrstuvwxyz";
@@ -38,9 +48,9 @@ function generate_string($chars = "abcdefghijklmnopqrstuvwxyz", $length = 6)
 {
     $chars_length = strlen($chars);
     $output = "";
-    for ($i=0; $i < $length; $i++) {
-      $randome_char = $chars[mt_rand(0, $chars_length - 1)];
-      $output .= $randome_char;
+    for ($i = 0; $i < $length; $i++) {
+        $randome_char = $chars[mt_rand(0, $chars_length - 1)];
+        $output .= $randome_char;
     }
 
     return $output;
@@ -48,74 +58,79 @@ function generate_string($chars = "abcdefghijklmnopqrstuvwxyz", $length = 6)
 
 function microtime_float()
 {
-  list($usec, $sec) = explode(" ", microtime());
-  return ((float)$usec + (float)$sec);
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float) $usec + (float) $sec);
 }
 
 function get_int_max()
 {
-    $max=0x7fff;
+    $max = 0x7fff;
     $probe = 0x7fffffff;
-    while ($max == ($probe>>16))
-    {
+    while ($max == ($probe >> 16)) {
         $max = $probe;
         $probe = ($probe << 16) + 0xffff;
     }
     return $max;
 }
 
-if (!defined('PHP_INT_MAX'))
-{
-    define ('PHP_INT_MAX', get_int_max());
+if (!defined('PHP_INT_MAX')) {
+    define('PHP_INT_MAX', get_int_max());
 }
 
 $max = array();
-$max[0]  = 2;
-$max[1]  = 10;
-$max[2]  = 100;
-$max[3]  = 1000;
-$max[4]  = 10000;
-$max[5]  = 100000;
-$max[6]  = 1000000;
-$max[7]  = 10000000;
-$max[8]  = 100000000;
-$max[9]  = 1000000000;
+$max[0] = 2;
+$max[1] = 10;
+$max[2] = 100;
+$max[3] = 1000;
+$max[4] = 10000;
+$max[5] = 100000;
+$max[6] = 1000000;
+$max[7] = 10000000;
+$max[8] = 100000000;
+$max[9] = 1000000000;
 $max[10] = PHP_INT_MAX;
-if ($max[10] > 10000000000)
-  $max[10] = 10000000000;
-
-function generate() {
-  global $max;
-  global $permited_chars;
-
-  $_SESSION['count']++;
-  $num = rand($max[$_SESSION['count']-1],
-              $max[$_SESSION['count']]-1);
-
-  $length = rand($max[$_SESSION['count']-1],
-              $max[$_SESSION['count']]-1);
-
-  $sub_permitted_chars = substr($permited_chars, 0, $_SESSION['count']+1);
-  $string = generate_string($sub_permitted_chars, $length);
-  $_SESSION['string'] = $string;
-
-  if ($_SESSION['count'] == 1)
-    $ans = prime($num);
-  else {
-    if ($num % 2 == 0)
-      $num++;
-    $ans = rand(0,999) < 500;
-    while (prime($num) != $ans || $num % 5 == 0)
-      $num += 2;
-  }
-
-  $_SESSION['number'] = $num;
-  if ($ans)
-    $_SESSION['answer'] = "yes";
-  else
-    $_SESSION['answer'] = "no";
+if ($max[10] > 10000000000) {
+    $max[10] = 10000000000;
 }
 
+function generate()
+{
+    global $max;
+    global $permited_chars;
+
+    $_SESSION['count']++;
+    $num = rand($max[$_SESSION['count'] - 1],
+        $max[$_SESSION['count']] - 1);
+
+    $length = rand($max[$_SESSION['count'] - 1],
+        $max[$_SESSION['count']] - 1);
+
+    $sub_permitted_chars = substr($permited_chars, 0, $_SESSION['count'] + 1);
+    $string = generate_string($sub_permitted_chars, $length);
+    $_SESSION['string'] = $string;
+
+    if ($_SESSION['count'] == 1) {
+        $ans = prime($num);
+    } else {
+        if ($num % 2 == 0) {
+            $num++;
+        }
+
+        $ans = rand(0, 999) < 500;
+        while (prime($num) != $ans || $num % 5 == 0) {
+            $num += 2;
+        }
+
+    }
+
+    $_SESSION['number'] = $num;
+    if ($ans) {
+        $_SESSION['answer'] = "yes";
+    } else {
+        $_SESSION['answer'] = "no";
+    }
+
+}
 
 ?>
 
@@ -132,15 +147,17 @@ function generate() {
 
 <?php
 if (!isset($_SESSION['count']) || isset($_SESSION['reset'])) {
-  $_SESSION['count'] = 0;
-  $_SESSION['wrong'] = 0;
-  $_SESSION['zero'] = microtime_float();
+    $_SESSION['count'] = 0;
+    $_SESSION['wrong'] = 0;
+    $_SESSION['zero'] = microtime_float();
 }
 printf("Session count is: %d", $_SESSION['count']);
-if (isset($_SESSION['generate']) || $_SESSION['count'] == 0)
+if (isset($_SESSION['generate']) || $_SESSION['count'] == 0) {
     generate();
-    unset($_SESSION['generate']);
-    unset($_SESSION['reset']);
+}
+
+unset($_SESSION['generate']);
+unset($_SESSION['reset']);
 ?>
 
 <h1>Find the longest palindrome!</h1>
@@ -168,16 +185,16 @@ if (isset($_SESSION['generate']) || $_SESSION['count'] == 0)
 <hr/>
 
 <p><span class="question">Question <?php echo "${_SESSION['count']}"; ?></span>:
-    length <?php echo strlen($_SESSION['string'])?>
+    length <?php echo strlen($_SESSION['string']) ?>
 
     <!-- Also add cheat answer here -->
     <?php if ($cheat) {
-            printf("<td width=\"16\">&mdash;</td>\n");
-            printf("<td>if I were you, I'd answer %d</td>\n", );//$_SESSION['answer']);
-        }?>
+    printf("<td width=\"16\">&mdash;</td>\n");
+    printf("<td>if I were you, I'd answer %d</td>\n", ); //$_SESSION['answer']);
+}?>
 
     <!-- Need to generate question here -->
-    <code class="block" id="question"><?php echo "${_SESSION['string']}"?></code>
+    <code class="block" id="question"><?php echo "${_SESSION['string']}" ?></code>
 
 
 </p>
@@ -192,60 +209,58 @@ if (isset($_SESSION['generate']) || $_SESSION['count'] == 0)
     </tr>
     </table>
 <?php
-      if (isset($_POST['answer'])) {
-        if ($_POST['answer'] == $_SESSION['answer'])
-          printf("<td><span class=\"correct\">RIGHT :)</span></td>\n");
-        else {
-          printf("<td><span class=\"wrong\">WRONG :(</span></td>\n");
-          $_SESSION['count']--;
-          $_SESSION['wrong']++;
-        }
-        if ($_SESSION['count'] < $limit) {
-          $_SESSION['generate'] = true;
-          printf("<td width=\"16\">&nbsp;</td>\n");
-          printf("<td><input type=\"submit\" name=\"continue\"
+if (isset($_POST['answer'])) {
+    if ($_POST['answer'] == $_SESSION['answer']) {
+        printf("<td><span class=\"correct\">RIGHT :)</span></td>\n");
+    } else {
+        printf("<td><span class=\"wrong\">WRONG :(</span></td>\n");
+        $_SESSION['count']--;
+        $_SESSION['wrong']++;
+    }
+    if ($_SESSION['count'] < $limit) {
+        $_SESSION['generate'] = true;
+        printf("<td width=\"16\">&nbsp;</td>\n");
+        printf("<td><input type=\"submit\" name=\"continue\"
                              id=\"continue\" value=\"Continue!\" /></td>\n");
-        }
-        else
-          $_SESSION['reset'] = true;
-      }
-      else {
-        //  What do we do if answer is not set?
-      }
+    } else {
+        $_SESSION['reset'] = true;
+    }
+
+} else {
+    //  What do we do if answer is not set?
+}
 ?>
     </form>
   </tr>
 </table>
 
 <?php
-  if (isset($_SESSION['reset'])) {
-?>
+if (isset($_SESSION['reset'])) {
+    ?>
 <p><span class="congratulations">Congra<a href="https://www.youtube.com/watch?v=1Bix44C1EzY">d</a>ulations!</span>
    You answered all questions!</p>
 <p>It took you
-  <?php printf("%0.3lf", microtime_float() - $_SESSION['zero']); ?> seconds
+  <?php printf("%0.3lf", microtime_float() - $_SESSION['zero']);?> seconds
 <?php
-  if ($_SESSION['wrong'] == 0) {
-?>
+if ($_SESSION['wrong'] == 0) {
+        ?>
   and you made no mistakes.
 <?php
-  }
-  else if ($_SESSION['wrong'] == 1) {
-?>
+} else if ($_SESSION['wrong'] == 1) {
+        ?>
   and you made one mistake
 <?php
-  }
-  else {
-?>
+} else {
+        ?>
   and you made <?php echo "${_SESSION['wrong']}" ?> mistakes.</p>
 <?php
-  }
-?>
+}
+    ?>
 <form <?php echo "action=\"$self\""; ?> id="r" name="r" method="post">
 <input type="submit" name="again" id="again" value="Play again!" />
 </form>
 <?php
-  }
+}
 ?>
 
 </body>
